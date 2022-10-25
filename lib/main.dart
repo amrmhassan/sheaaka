0310928dd59c1111c3987495d5d2401d8f6c2251
amrmhassan <amrmhassanmarsafa@gmail.com>
2022-10-25 15:07:36 +0200

    turning to windows dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+// test github
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project/constants/colors.dart';
